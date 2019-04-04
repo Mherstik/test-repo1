0a7e68ab5cd0f252481ad7cb1e46510c06fec60b
Marcus Herstik <marcus@herstik.com>
2019-04-05 10:23:36 +1100

    1
    1

--- hello.py
+++ hello.py
@@ -3,3 +3,11 @@
 # This is a python test
 print("Hello World")
 
+# This is a new section
+
+name = input("What is your name? )
+if name == "Marcus":
+	print("You rock!")
+else:
+	print('You Suck!!')
+
